5f7419758bb394a84614cf08452e4dcfa23c4c4d
neko2016 <honglok16@hotmail.com>
2017-04-11 14:03:06 +0100

    added documentation

--- discrete_time_series.r
+++ discrete_time_series.r
@@ -1,21 +1,21 @@
 library(ggplot)
 setwd("path_to_working_folder")
-getwd()
+# getwd()
 
 df <- read.csv(file="example.csv",
       stringsAsFactors = FALSE)
 
-head(df)
-summary(df)
-str(df)
+# head(df)
+# summary(df)
+# str(df)
 
 # subset the data by "reading_type"
 newdata1 <- df[which(df$reading_type == 1), ]
 
-# time series on a continuous x-axis
+# time series on a continuous x-axis ("secs_elapsed")
 line1 <- ggplot(newdata1, aes(x = newdata1$secs_elapsed)) + geom_line(aes(y= newdata1$reading_value))
 
-# scatter plot on a continuous x-axis
+# scatter plot on a continuous x-axis ("secs_elapsed")
 scatter1 <- ggplot(newdata1, aes(x = newdata1$secs_elapsed, y = newdata1$reading_value)) + geom_point()
 
 # display the data by exhalation "seid" on a discontinuous x-axis
